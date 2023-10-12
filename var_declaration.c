5faa0cb729c295a6cf353048c72db4cb7109b935
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-10-12 23:23:00 +0100

    Added a the introductory comment.

---
+++ var_declaration.c
@@ -0,0 +1,6 @@
+/**********************************************************
+Write a program which computes and prints the sum of the
+days in all four quarters of the current year.
+Author: Astrojoe
+10th October 2023
+**********************************************************/
