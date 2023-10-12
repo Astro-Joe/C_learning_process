e649eed02e0058488a09739e778810ea6b2799d8
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-10-12 23:43:28 +0100

    Declared all necessary variables.

--- var_declaration.c
+++ var_declaration.c
@@ -4,3 +4,20 @@ days in all four quarters of the current year.
 Author: Astrojoe
 10th October 2023
 **********************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int days_january = 31;
+    int days_feburary = 28;
+    int days_march = 31;
+    int days_april = 30;
+    int days_may = 31;
+    int days_june = 30;
+    int days_july = 31;
+    int days_august = 31;
+    int days_september = 30;
+    int days_october = 31;
+    int days_november = 30;
+    int days_december = 31;
+}
