1e6f561577cd5822b53d46030849fce5a06ae849
Astro-Joe <ilemobayojoseph06@gmail.com>
2023-10-12 23:47:33 +0100

    Declared 4 more variables.

--- var_declaration.c
+++ var_declaration.c
@@ -20,4 +20,11 @@ int main()
     int days_october = 31;
     int days_november = 30;
     int days_december = 31;
+
+    int Q1 = days_january + days_feburary + days_march
+    int Q2 = days_april + days_may + days_june
+    int Q3 =  days_july + days_august + days_september
+    int Q4 =  days_october +days_november + days_december
+
+    
 }
